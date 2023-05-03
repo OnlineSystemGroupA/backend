1021e606dfae1fc69bb5f8e7d3f177fb9434415f
Kekwy <keekkewy@qq.com>
2023-05-03 22:59:11 +0800

    feat: 添加 OpenAPI 在线文档的配置

---
+++ server/src/main/java/com/stcos/server/config/openapi/OpenAPIConfig.java
@@ -0,0 +1,42 @@
+package com.stcos.server.config.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+
+/**
+ * description
+ *
+ * @author Kekwy
+ * @version 1.0
+ * @since 2023/4/4 10:45
+ */
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info()
+                        .title("南大软件测试中心在线系统")
+                        .description("后端 API 文档")
+                        .contact(new Contact()
+                                .name("在线系统项目 A 组")
+                                .url("https://github.com/OnlineSystemGroupA")
+                                .email("support@example.com"))
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(
+                        new ArrayList<>() {{
+                            add(new Server().url("http://localhost:8081/"));
+                        }}
+                );
+    }
+}
