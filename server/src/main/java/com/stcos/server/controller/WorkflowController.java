48182e1a4ed1b4fa1fe2a726d2ffccf0f2caedf3
Kekwy <keekkewy@qq.com>
2023-05-09 14:01:11 +0800

    fix(WorkflowController): 修正接口变动引入的错误

--- server/src/main/java/com/stcos/server/controller/WorkflowController.java
+++ server/src/main/java/com/stcos/server/controller/WorkflowController.java
@@ -2,9 +2,12 @@ package com.stcos.server.controller;
 
 import com.stcos.server.controller.api.WorkflowApi;
 import com.stcos.server.pojo.dto.ProcessIdDto;
+import com.stcos.server.pojo.dto.SamplePathDto;
 import com.stcos.server.pojo.dto.TaskDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,17 +19,23 @@ import java.util.Optional;
  * @version 1.0
  * @since 2023/5/5 20:45
  */
+
+@RestController
 public class WorkflowController implements WorkflowApi {
 
+    @Override
+    public ResponseEntity<List<MultipartFile>> downloadSamples(String processId) {
+        return WorkflowApi.super.downloadSamples(processId);
+    }
+
+    @Override
+    public ResponseEntity<List<SamplePathDto>> samplesUpload(String processId, List<MultipartFile> files) {
+        return WorkflowApi.super.samplesUpload(processId, files);
+    }
 
     @Override
     public ResponseEntity<Void> completeTask(String taskId) {
         return WorkflowApi.super.completeTask(taskId);
-    }
-
-    @Override
-    public ResponseEntity<Object> downloadSamples(String processId) {
-        return WorkflowApi.super.downloadSamples(processId);
     }
 
     @Override
@@ -44,10 +53,6 @@ public class WorkflowController implements WorkflowApi {
         return WorkflowApi.super.getTasks();
     }
 
-    @Override
-    public ResponseEntity<Void> samplesUpload(String processId) {
-        return WorkflowApi.super.samplesUpload(processId);
-    }
 
     @Override
     public ResponseEntity<ProcessIdDto> startProcess() {
