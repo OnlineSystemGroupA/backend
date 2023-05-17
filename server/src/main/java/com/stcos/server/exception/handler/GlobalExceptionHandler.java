8690b69716f3351e7d85057a81eab5efedd0ad74
Kekwy <keekkewy@qq.com>
2023-05-17 14:42:37 +0800

    feat: 新增 ServerErrorException 异常类型以及全局异常响应器
    ServerErrorException: 后端遇到无法恢复，需要结束请求的异常时抛出

---
+++ server/src/main/java/com/stcos/server/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.stcos.server.exception.handler;
+
+import com.stcos.server.exception.ServerErrorException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * controller 增强器，目前主要用于处理异常
+ *
+ * @author Kekwy
+ * @version 1.0
+ * @since 2023/5/17 14:34
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ResponseBody
+    @ExceptionHandler(value = ServerErrorException.class)
+    public ResponseEntity<String> errorHandler(ServerErrorException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
