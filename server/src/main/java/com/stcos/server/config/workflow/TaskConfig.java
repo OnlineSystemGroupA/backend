bc884207e5e6fd17157efece0ebcc4c119da524a
Mikeal-C <100679612+Mikeal-C@users.noreply.github.com>
2023-05-18 13:50:42 +0800

    feat: 添加了taskRequiredVarMap

--- server/src/main/java/com/stcos/server/config/workflow/TaskConfig.java
+++ server/src/main/java/com/stcos/server/config/workflow/TaskConfig.java
@@ -3,7 +3,6 @@ package com.stcos.server.config.workflow;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,9 +21,18 @@ public class TaskConfig {
     @Bean
     public Map<String, List<String>> taskRequiredVarMap() {
         return new HashMap<>() {{
-           put("填写委托", List.of("ApplicationForm"));
-           // TODO 定义完成每个任务所需要的资源
-//           put("分配工作人员", List.of("Asdfjkhasdafas"));
+            put("填写委托", List.of("ApplicationForm"));
+            put("分配工作人员", List.of("Assignee"));  //被分配的工作人员
+            put("审核用户委托", List.of("ApplicationVerifyForm"));
+            put("用户修改委托", List.of("ApplicationForm"));
+            put("生成报价", List.of("Quotation")); //报价
+            put("生成合同草稿", List.of("ContractDraft")); //合同草稿
+            put("签署合同", List.of("Contract")); //合同
+            put("上传样品", List.of("Sample")); //样品
+            put("生成测试方案", List.of("TestPlanForm"));
+            put("生成测试报告", List.of("TestReportForm"));
+            put("用户确认测试结果", List.of("ReportVerifyForm"));
+            put("后续处理", List.of("DocumentReviewForm"));
         }};
     }
 
